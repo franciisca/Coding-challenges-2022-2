56431da181c07b72d2aca517576872db4d730de4
Fran <frannii.sv@gmail.com>
2022-06-19 21:37:50 +0900

    coding challenge

---
+++ CC 2022-06-19/main.js
@@ -0,0 +1,40 @@
+//https://www.codewars.com/kata/55d1d6d5955ec6365400006d/javascript
+//Round up to the next multiple of 5
+
+// Given an integer as input, can you round it to the next (meaning, "higher") multiple of 5?
+
+// Examples:
+
+// input:    output:
+// 0    ->   0
+// 2    ->   5
+// 3    ->   5
+// 12   ->   15
+// 21   ->   25
+// 30   ->   30
+// -2   ->   0
+// -5   ->   -5
+// etc.
+// Input may be any positive or negative integer (including 0).
+
+// You can assume that all inputs are valid integers.
+
+//P. takes an integer (positive, negative and also 0)
+//R. returns the closest "next" integer multiple of 5.
+//E. 18 => 20 ; -5 => -5; 21 => 25
+//Pseudo. write a function that takes an integer. 
+// if it is % 5 === 0, return the same number.}
+//else if, is 0, return 5
+//else round it up with Math.ceil(number%5 === 0)
+
+function roundUp5 (num){
+    if(num === 0){
+        return 5
+    }else if (num%5=== 0){
+        return num
+    }else{
+        return Math.ceil(num%5 === 0)
+    }
+}
+
+roundUp5(20)
