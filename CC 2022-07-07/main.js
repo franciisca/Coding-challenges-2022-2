808a6278c021ed0229f8212a96c2f9391b27af37
Fran <frannii.sv@gmail.com>
2022-07-07 10:17:58 +0900

    coding challenge

---
+++ CC 2022-07-07/main.js
@@ -0,0 +1,20 @@
+// https://www.codewars.com/kata/551b4501ac0447318f0009cd/train/javascript
+//Boolean to String
+
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+
+//P. takes a boolean only (true or false statement)
+//R. returns "true" or "false" (string)
+//E. true => "true" ... false => "false"
+//Pseudo. function takes "boolean" as a parameter. returns the boolean.toString()
+
+function booleanToString(boolean){
+    return boolean.toString()
+  }
+  
+  booleanToString(true)
+
+  //works!
