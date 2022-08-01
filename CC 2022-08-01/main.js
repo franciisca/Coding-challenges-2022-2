6ea38b479f0ee6d4c1afb365df4d9b4a9228c064
Fran <frannii.sv@gmail.com>
2022-08-01 11:45:25 +0900

    coding challenge

--- CC 2022-08-01/main.js
+++ CC 2022-08-01/main.js
@@ -4,7 +4,11 @@
 //The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
 // What if the string is empty? Then the result should be empty object literal, {}.
 
-//P.
-//R.
-//E.
-//Pseudo.
+//P. We have a string. Doesn't consider uppercase/lowcase. If the string is empty it returns {}
+//R. returns the number of characters in the string, separating them according to which character it is. Returns the result in an object. If string is empty, returns an empty object,
+//E. "abba" returns {'a':2, 'b':2}. "" returns {}
+//Pseudo. write a function that takes a string. 
+//create a variable that hold and object ({})
+//loop. Make the string an array and loop through it. use if statements and indexOf. 
+//Then
+
