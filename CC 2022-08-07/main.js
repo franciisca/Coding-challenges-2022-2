aef8c0a5ec5b3511fc4f2ed3a9f917cff3d318b4
Fran <frannii.sv@gmail.com>
2022-08-09 09:54:29 +0900

    coding challenge

--- CC 2022-08-07/main.js
+++ CC 2022-08-07/main.js
@@ -7,16 +7,21 @@
   //R. return true if the string is the same when turned backwards. If not, return false.
   //E. "level" => true
   // "hello" => false
-  //Pseudo. Write a function that takes a string. set it to lower case. check if it the string equals the same string backwards (reverse and join methods). Return true if it is.
+  //Pseudo. Write a function that takes a string. set it to lower case. check if it the string equals the same string backwards (turn it into array and aply the reverse and join methods). Return true if it is.
 
 
-  function isPalindrome(x) {
+function isPalindrome(x) {
     let xLowerCase = x.toLowerCase()
-    if (xLowerCase == xLowerCase.reverse().join("")){
+    if (xLowerCase === xLowerCase.split('').reverse().join("")){
         return true
     }else{
         return false
     }
   }
+isPalindrome("level")
 
-  isPalindrome("level")
+//Works!! More simplified version:
+
+function isPalindrome(x){
+    return x.toLowerCase() === x.toLowerCase().split("").reverse().join("")
+}
