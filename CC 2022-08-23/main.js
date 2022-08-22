b482017fd27c8fe10905850d5ec95499b78f0e18
Fran <frannii.sv@gmail.com>
2022-08-22 22:54:53 +0900

    coding challenge

---
+++ CC 2022-08-23/main.js
@@ -0,0 +1,14 @@
+// You will be given a list of strings. You must sort it alphabetically (case-sensitive, and based on the ASCII values of the chars) and then return the first value.
+
+// The returned value must be a string, and have "***" between each of its letters.
+
+// You should not remove or add elements from/to the array.
+
+//P. Takes a list of strings (in array)
+//R. sort them alphabetically (case sensitive and according to ASCII). Return the FIRST VALUE. Returned value must be a string and have "***" in between each of its letters.
+//E. ["hola", "como", "vamos"] => 
+//Pseudo. write a function that takes an array. return arr.sort()
+
+function firstValue (arr){
+    return arr.sort()
+}
