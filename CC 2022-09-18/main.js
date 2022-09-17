6fb084e468bce564d0725a2998cfc69736fde63c
Fran <frannii.sv@gmail.com>
2022-09-17 17:31:38 +0900

    coding challenge

---
+++ CC 2022-09-18/main.js
@@ -0,0 +1,9 @@
+//Function 2 - squaring an argument
+//https://www.codewars.com/kata/523b623152af8a30c6000027/train/javascript
+
+//Now you have to write a function that takes an argument and returns the square of it.
+
+//P.
+//R.
+//E.
+//pseudo.
