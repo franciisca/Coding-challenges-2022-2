c254f11960798a08102304e20cb9e5414b30451a
Fran <frannii.sv@gmail.com>
2022-09-18 17:40:16 +0900

    coding challenge

--- CC 2022-09-18/main.js
+++ CC 2022-09-18/main.js
@@ -3,7 +3,11 @@
 
 //Now you have to write a function that takes an argument and returns the square of it.
 
-//P.
-//R.
-//E.
-//pseudo.
+//P. function takes an argument (integers only?)
+//R. returns the square of that argument.
+//E. 2 => returns 4
+//pseudo. function takes an argument. returns it squared. Use Math.pow(number,2)
+
+function square(number){
+    return Math.pow(number,2)
+}
