99ec94cf7c6885c1d17bf5ed1f86ad901d068975
Fran <frannii.sv@gmail.com>
2022-09-20 19:47:20 +0900

    coding challenge

--- CC 2022-09-19/main.js
+++ CC 2022-09-19/main.js
@@ -10,7 +10,24 @@
 // The result may contain fives. ;-)
 // The start number will always be smaller than the end number. Both numbers can be also negative!
 
-//P.
-//R.
-//E.
-//pseudo.
+//P. function takes two arguments (two integers, positive or negative). The first one will always be smaller than the second one.
+//R. return the sum of the integers between the arguments (including the arguments) but excluding from the sum any integer containing "5". 
+//E.2,9 -> 2,3,4,6,7,8,9 -> returns 39
+//pseudo.function has two parameters (negative or positive integers). add the numbers in between them (including the arguments) except for integers containing 5.
+
+function sumWithoutFive(start,end){
+    let arr = [start,end]
+    let sum = arr.map(index => {
+
+    })
+}
+
+function sumWithoutFive(start,end){
+    let arr = [start,end]
+    arr.filter((n) => {
+
+    })
+    return arr.reduce((a, b) => a + b, 0)
+}
+
+sumWithoutFive(2,9)
